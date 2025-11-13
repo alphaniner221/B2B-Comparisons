4a0cde212e292de19886850de6948b13736817e2
alphaniner221 <github.com2026@outlook.com>
2025-11-13 17:45:01 +0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Toggle mobile menu
-const menuBtn = document.getElementById('menu-btn');
-const mobileMenu = document.getElementById('mobile-menu');
-
-menuBtn.addEventListener('click', () => {
-  mobileMenu.classList.toggle('hidden');
-});
